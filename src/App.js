1c0f757d629a0f5dd663809f447e702993813078
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-24 21:17:53 +0300

    Реализовал хранение данных в localStorage

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import Filter from './components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: uuidv4(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: uuidv4(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: uuidv4(), name: 'Eden Clements', number: '645-17-79' },
-      { id: uuidv4(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContact = data => {
     const contact = {
